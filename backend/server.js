01d463bfb9e5661d0fa71509fbb02058bbed2060
Neha91-svg <ng941628@gmail.com>
2025-12-28 13:05:28 +0530

    added route

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/workflows', workflowRoutes);
-app.use("/api/workflows/subscription-reminder", subscriptionReminderWorkflow);
+app.use("/api/workflows", subscriptionReminderWorkflow);
 
 app.get("/", (req, res) => {
     res.send("API is running");
