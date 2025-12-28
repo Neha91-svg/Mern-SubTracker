df0ba90a50a08fc4ebb86fc6ba9622ae73db11d3
Neha91-svg <ng941628@gmail.com>
2025-12-28 13:14:46 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,66 +1,60 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
 
-const app = express();
+import express from 'express';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
-import { sendEmail } from "./utils/sendEmail.js";
-
 import subscriptionRoutes from './routes/subscriptionRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import arcjetMiddleware from "./middleware/arcjetMiddleware.js";
-
-import cookieParser from 'cookie-parser';
-import errorHandler from './middleware/errorMiddleware.js';
 import workflowRoutes from "./routes/workflowRoutes.js";
-import cors from "cors";
+import errorHandler from './middleware/errorMiddleware.js';
 
 import { subscriptionReminderWorkflow } from "./workflows/subscriptionReminder.workflow.js";
 
-
-
-
-
+const app = express();
 connectDB();
+
 app.use(cors({
     origin: [
-        "https://mern-sub-tracker.vercel.app",   // 👈 MAIN PROD
+        "https://mern-sub-tracker.vercel.app",
         "http://localhost:5173",
         "https://mern-sub-tracker-git-main-neha91-svgs-projects.vercel.app"
     ],
     credentials: true,
 }));
 
-
-
 app.set("trust proxy", 1);
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-app.use(arcjetMiddleware);
 
+// routes
 app.use('/api/users', userRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/workflows', workflowRoutes);
-app.use("/api/workflows", subscriptionReminderWorkflow);
+
+// ✅ WORKFLOW (single correct mount)
+app.use(
+    "/api/workflows/subscription-reminder",
+    subscriptionReminderWorkflow
+);
 
 app.get("/", (req, res) => {
     res.send("API is running");
 });
 
-// 404 handler
+// 404
 app.use((req, res, next) => {
     res.status(404);
     next(new Error("Route not found"));
 });
 
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
