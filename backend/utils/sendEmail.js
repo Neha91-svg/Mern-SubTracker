5cc41e95f40a3d4e5dfd6e6e59020b32db609d0c
Neha91-svg <ng941628@gmail.com>
2025-12-29 15:58:37 +0530

    add

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -12,7 +12,7 @@ if (!process.env.SMTP_USER || !process.env.SMTP_PASS) {
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
